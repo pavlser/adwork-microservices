3706d56104b81a0242e9474efe9e0c7d5454c6ee
pavlser <ergo77@gmail.com>
2018-11-26 17:19:08 +0200

    Add zuul gateway app

---
+++ zuul-gateway-app/src/main/java/com/adwork/microservices/gateway/ZuulGatewayAppApplication.java
@@ -0,0 +1,12 @@
+package com.adwork.microservices.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulGatewayAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayAppApplication.class, args);
+	}
+}
